006512d6d51efda8fb2fdf3e981cd76e2d8885e1
Eric Bortz <e.bortz@netstack.de>
2023-04-17 12:06:24 +0200

    added module to get tags

---
+++ src/Api/Tag.php
@@ -0,0 +1,43 @@
+/*
+ * Tag.php
+ * @author Eric Bortz <e.bortz@netstack.de>
+ * @copyright 2023 Eric Bortz
+ */
+
+
+namespace NetstackDE\LaravelSevdeskApi\Api;
+
+use NetstackDE\LaravelSevdeskApi\Api\Utils\ApiClient;
+use NetstackDE\LaravelSevdeskApi\Api\Utils\Routes;
+use Illuminate\Support\Collection;
+
+/**
+ * Sevdesk Contact Api
+ *
+ * @see https://api.sevdesk.de/#tag/Tag
+ */
+
+class StaticCountry extends ApiClient
+{
+    /**
+     * Return all tags.
+     *
+     * @return mixed
+     */
+    public function all(int $limit = 1000)
+    {
+        return Collection::make($this->_get(Routes::TAG, ['limit' => $limit]));
+    }
+
+    /**
+     * Return a single tag.
+     *
+     * @param $contryId
+     * @return mixed
+     */
+    public function get($tagId)
+    {
+        return $this->_get(Routes::TAG. '/' . $tagId)[0];
+    }
+
+}
